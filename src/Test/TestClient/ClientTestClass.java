c95402a76f322a1d98591d121d0333440aa7831a
Chris Adams <chrisadams1267@gmail.com>
2012-11-03 18:10:38 +0000

    Adding Client & Server Test Classes

---
+++ src/Test/TestClient/ClientTestClass.java
@@ -0,0 +1,31 @@
+package Test.TestClient;
+
+import com.mycompany.reservationsystem.peer.client.PeerClient;
+import com.mycompany.reservationsystem.peer.client.booking.BookingClient;
+import com.mycompany.reservationsystem.peer.deamon.PeerStateDeamon;
+
+public class ClientTestClass {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		PeerStateDeamon peerDeamon = new PeerStateDeamon();
+		PeerClient peerClient = new PeerClient();
+		BookingClient bookingClient = new BookingClient();
+		
+		peerDeamon.start();
+		peerClient.start();
+		bookingClient.start();
+		
+		try {
+			System.out.println("Hello");
+			Thread.currentThread();
+			Thread.sleep(100000);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		peerDeamon.stop();
+		peerClient.stop();
+		bookingClient.stop();
+	}
+}
---
+++ src/Test/TestClient/ServerTestClass.java
@@ -0,0 +1,31 @@
+package Test.TestClient;
+
+import com.mycompany.reservationsystem.peer.deamon.PeerStateDeamon;
+import com.mycompany.reservationsystem.peer.server.PeerServer;
+import com.mycompany.reservationsystem.peer.server.booking.BookingServer;
+
+public class ServerTestClass {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		PeerStateDeamon peerDeamon = new PeerStateDeamon();
+		PeerServer peerServer = new PeerServer();
+		BookingServer bookingServer = new BookingServer();
+		
+		peerDeamon.start();
+		peerServer.start();
+		bookingServer.start();
+		
+		try {
+			System.out.println("Hello");
+			Thread.currentThread();
+			Thread.sleep(100000);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		peerDeamon.stop();
+		peerServer.stop();
+		bookingServer.stop();
+	}
+}
