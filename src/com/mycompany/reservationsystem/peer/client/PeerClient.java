5b9fea98a22c75253d9a272e97a243ea7d2dae74
Chris Adams <chrisadams1267@gmail.com>
2012-11-06 21:33:56 +0000

    Client remove warnings

--- src/com/mycompany/reservationsystem/peer/client/PeerClient.java
+++ src/com/mycompany/reservationsystem/peer/client/PeerClient.java
@@ -1,13 +1,7 @@
 package com.mycompany.reservationsystem.peer.client;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Date;
 
-import com.mycompany.reservationsystem.peer.communication.CommunicationMessages;
 import com.mycompany.reservationsystem.peer.data.Database;
 import com.mycompany.reservationsystem.peer.data.Peer;
 
@@ -21,14 +15,7 @@ import com.mycompany.reservationsystem.peer.data.Peer;
         Spawn a client worker thread
     End
  */
-public class PeerClient extends Thread{
-	private static final int PORT_NUMBER = 50000;
-	private Socket requestSocket;
-	private ObjectOutputStream out;
-	private ObjectInputStream in;
-	private String ipAddress;
- 	private boolean isFinished;
-	
+public class PeerClient extends Thread{	
 	public void run(){
 		//System.out.println("PeerClient");
 		Database peerTable = Database.getInstance();
@@ -42,92 +29,6 @@ public class PeerClient extends Thread{
 				//findPeers();
 				new PeerClientWorker(peer.getPeerIpAddress()).start();
 			}
-		}
-	}
-	
-	private void setIPAddress(String ipAddress){
-		this.ipAddress = ipAddress;
- 		this.isFinished = false;
-	}
-	
-	private void findPeers(){
-		try{
-			requestSocket = new Socket(this.ipAddress, PORT_NUMBER);
-			
-			out = new ObjectOutputStream(requestSocket.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(requestSocket.getInputStream());
-			
-			while(isFinished == false){
-				sendMessage(CommunicationMessages.IP_REQUEST);
-				
-				String message = (String) in.readObject();
-				System.out.println("Got message " + message);
-				if(message.startsWith(CommunicationMessages.IP_RESPONSE.toString())){
-					/*
-					 * Message with no ip address shows that server has given all ip address, 
-					 * server will return IP_RESPONSE: (12 chars)
-					 */
-					if(message.length() != 12){
-						String ipAddress = message.substring(message.indexOf(":")+1, message.length());
-						
-						Database peerTable = Database.getInstance();
-						//Check if ip address is present, if ip address isn't present then will return null
-						if(peerTable.findPeerByIpAddress(ipAddress) == null){
-							//Add ip address to DB
-							Peer newPeer = new Peer(ipAddress,Peer.STATE.INACTIVE, new Date().getTime());
-							peerTable.addPeer(newPeer);
-						}
-					}
-					else{
-						isFinished = true;
-					}
-					
-				}
-			}
-			
-			
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		catch(IOException ioException){
-			ioException.printStackTrace();
-			Peer peer = new Peer();
-			peer = Database.getInstance().findPeerByIpAddress(ipAddress);
-			peer.setState(Peer.STATE.INACTIVE);
-			peer.setEpochTime(new Date().getTime());
-			Database.getInstance().updatePeer(peer);
-		}
-		finally{
-			//Closing connection
-			try{
-				in.close();
-				out.close();
-				requestSocket.close();
-			}
-			catch(NullPointerException e){
-				
-			}
-			catch(IOException ioException){
-				ioException.printStackTrace();
-			}
-		}
-	}
-	
-	private void sendMessage(CommunicationMessages communicationMessage){
-		String message = "";
-		if(communicationMessage.toString().equals(CommunicationMessages.IP_REQUEST.toString())){
-			message += CommunicationMessages.IP_REQUEST.toString();
-		}
-		
-		try{
-			out.writeObject(message);
-			out.flush();
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
 		}
 	}
 }
--- src/com/mycompany/reservationsystem/peer/client/booking/BookingClient.java
+++ src/com/mycompany/reservationsystem/peer/client/booking/BookingClient.java
@@ -1,15 +1,8 @@
 package com.mycompany.reservationsystem.peer.client.booking;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Date;
 
-import com.mycompany.reservationsystem.peer.communication.CommunicationMessages;
 import com.mycompany.reservationsystem.peer.data.Database;
-import com.mycompany.reservationsystem.peer.data.FlightBooking;
 import com.mycompany.reservationsystem.peer.data.Peer;
 
 /*
@@ -23,13 +16,6 @@ import com.mycompany.reservationsystem.peer.data.Peer;
     End
  */
 public class BookingClient extends Thread{
-	private static final int PORT_NUMBER = 50001;
-	private Socket requestSocket;
-	private ObjectOutputStream out;
-	private ObjectInputStream in;
- 	private String ipAddress;
- 	private boolean isFinished;
-	
 	public void run(){
 		Database peerTable = Database.getInstance();
 		
@@ -42,143 +28,4 @@ public class BookingClient extends Thread{
 			}
 		}
 	}
-	
-	private void setIPAddress(String ipAddress){
-		this.ipAddress = ipAddress;
- 		this.isFinished = false;
-	}
-	
-	private void findBookings(){
-		try{
-			requestSocket = new Socket(this.ipAddress, PORT_NUMBER);
-			
-			out = new ObjectOutputStream(requestSocket.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(requestSocket.getInputStream());
-			
-			while(isFinished == false){
-				sendMessage(CommunicationMessages.TRANSACTION_REQUEST);
-				
-				String message = (String) in.readObject();
-				System.out.println("Got message " + message);
-				if(message.startsWith(CommunicationMessages.TRANSACTION_RESPONSE.toString())){
-					/*
-					 * Message with no booking shows that server has given all bookings, 
-					 * server will return TRANSACTION_RESPONSE: (21 chars)
-					 */
-					System.out.println("Got message " + message);
-					
-					if(message.length() != 21){ //Got a booking
-						String dataPartOfMessage = message.substring(message.indexOf(":")+1, message.length());
-						FlightBooking booking = parseBookingMessage(dataPartOfMessage);
-						
-						Database flightTable = Database.getInstance();
-						if(flightTable.isFlightBooking(booking.getTransactionTime(), booking.getEmail()) == false){
-							flightTable.addBooking(booking);
-						}
-					}
-					else{ //Got blank booking
-						isFinished = true;
-					}
-				}
-			}
- 		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		catch(IOException ioException){
-			ioException.printStackTrace();
-			Peer peer = new Peer();
-			peer = Database.getInstance().findPeerByIpAddress(ipAddress);
-			peer.setState(Peer.STATE.INACTIVE);
-			peer.setEpochTime(new Date().getTime());
-			Database.getInstance().updatePeer(peer);
-		}
-		finally{
-			//Closing connection
-			try{
-				in.close();
-				out.close();
-				requestSocket.close();
-			}
-			catch(IOException ioException){
-				ioException.printStackTrace();
-			}
-			catch (NullPointerException e) {
-			}
-		}
-	}
-	
-	private void sendMessage(CommunicationMessages communicationMessage){
-		String message = "";
-		if(communicationMessage.toString().equals(CommunicationMessages.TRANSACTION_REQUEST.toString())){
-			message += CommunicationMessages.TRANSACTION_REQUEST.toString();
-		}
-		
-		try{
-			out.writeObject(message);
-			out.flush();
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-	}
- 	
- 	//Sample data = 1351858413975,bob@gmail.com,31/08/2012@1200,NA,0,1,0,REQUESTED,
- 	private FlightBooking parseBookingMessage(String dataPartOfMessage){
- 		FlightBooking booking = new FlightBooking();
- 		
- 		String transactionEpoch = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		dataPartOfMessage = dataPartOfMessage.substring(dataPartOfMessage.indexOf(",")+1, dataPartOfMessage.length());
- 		String email = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		dataPartOfMessage = dataPartOfMessage.substring(dataPartOfMessage.indexOf(",")+1, dataPartOfMessage.length());
- 		String flightToCityAt = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		dataPartOfMessage = dataPartOfMessage.substring(dataPartOfMessage.indexOf(",")+1, dataPartOfMessage.length());
- 		String flightToCampAt = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		dataPartOfMessage = dataPartOfMessage.substring(dataPartOfMessage.indexOf(",")+1, dataPartOfMessage.length());
- 		String fromCity = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		dataPartOfMessage = dataPartOfMessage.substring(dataPartOfMessage.indexOf(",")+1, dataPartOfMessage.length());
- 		String fromCamp = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		dataPartOfMessage = dataPartOfMessage.substring(dataPartOfMessage.indexOf(",")+1, dataPartOfMessage.length());
- 		String price = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		dataPartOfMessage = dataPartOfMessage.substring(dataPartOfMessage.indexOf(",")+1, dataPartOfMessage.length());
- 		String state = dataPartOfMessage.substring(0,dataPartOfMessage.indexOf(","));
- 		
- 		System.out.println(transactionEpoch + " " + email);
- 		
- 		booking.setTransactionTime(Long.parseLong(transactionEpoch));
- 		booking.setEmail(email);
- 		booking.setFlightToCityAt(flightToCityAt);
- 		booking.setFlightToCampAt(flightToCampAt);
- 		booking.setPrice(Double.parseDouble(price));
- 		
- 		if(Integer.parseInt(fromCity) == 1){
- 			booking.setFromCity(true);
- 		}
- 		else{
- 			booking.setFromCity(false);
- 		}
- 		
- 		if(Integer.parseInt(fromCamp) == 1){
- 			booking.setFromCamp(true);
- 		}
- 		else{
- 			booking.setFromCamp(false);
- 		}
- 		
- 		if(state.equals(FlightBooking.STATE.REQUESTED.toString())){
- 			booking.setState(FlightBooking.STATE.REQUESTED);
- 		}
- 		else if(state.equals(FlightBooking.STATE.CONFIRMED.toString())){
- 			booking.setState(FlightBooking.STATE.CONFIRMED);
- 		}
- 		else if(state.equals(FlightBooking.STATE.CANCEL.toString())){
- 			booking.setState(FlightBooking.STATE.CANCEL);
- 		}
- 		else if(state.equals(FlightBooking.STATE.CANCELED.toString())){
- 			booking.setState(FlightBooking.STATE.CANCELED);
- 		}
- 		return booking;
- 	}
 }
