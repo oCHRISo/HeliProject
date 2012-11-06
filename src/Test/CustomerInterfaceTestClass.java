6d17735ee2f8a082032ab9618d7c5a3bb6ff137a
Chris Adams <chrisadams1267@gmail.com>
2012-11-06 12:36:29 +0000

    Adding customer interface test class

---
+++ src/Test/CustomerInterfaceTestClass.java
@@ -0,0 +1,10 @@
+package Test;
+
+import com.mycompany.reservationsystem.peer.ui.CustomerUserInterface;
+
+public class CustomerInterfaceTestClass {
+	public static void main(String[] args) {
+		CustomerUserInterface cui = new CustomerUserInterface();
+		cui.showOptions();
+	}
+}
