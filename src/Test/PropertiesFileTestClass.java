7015215a29a147b7a9f4e069cb9419d116373dd5
Chris Adams <chrisadams1267@gmail.com>
2012-11-07 16:22:50 +0000

    Allowing ip addresses to be stored in config

--- src/Test/PropertiesFileTestClass.java
+++ src/Test/PropertiesFileTestClass.java
@@ -1,5 +1,7 @@
 package Test;
 
+import java.util.ArrayList;
+
 import com.mycompany.reservationsystem.peer.data.PropertieFile;
 
 public class PropertiesFileTestClass {
@@ -7,5 +9,11 @@ public class PropertiesFileTestClass {
 		PropertieFile pF = new PropertieFile();
 		System.out.println(pF.getCompanyNumber());
 		System.out.println(pF.getTimeToWait());
+		ArrayList<String> companiesIpAddresses = pF.getCompaniesIpAddresses();
+		
+		for(String ip : companiesIpAddresses){
+			System.out.println(ip);
+		}
+		
 	}
 }
--- src/com/mycompany/reservationsystem/peer/data/PropertieFile.java
+++ src/com/mycompany/reservationsystem/peer/data/PropertieFile.java
@@ -2,6 +2,7 @@ package com.mycompany.reservationsystem.peer.data;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class PropertieFile {
@@ -28,4 +29,32 @@ public class PropertieFile {
 	public String getTimeToWait(){
 		return prop.getProperty("TimeToWait");
 	}
+	
+	public ArrayList<String> getCompaniesIpAddresses(){
+		return parseIpAddresses();
+	}
+	
+	//Sample data = 192.168.1.1,192.168.1.2,192.168.1.3,
+ 	private ArrayList<String> parseIpAddresses(){
+ 		String ipAddresses = prop.getProperty("CompanyServerIPAddresses");
+ 		
+ 		int numberOfIpAddresses = 0;
+ 		for(int i = 0; i < ipAddresses.length(); i++){
+ 			if(ipAddresses.charAt(i) == ','){
+ 				numberOfIpAddresses++;
+ 			}
+ 		}
+ 		
+ 		ArrayList<String> listOfIpAddresses = new ArrayList<String>();
+ 		do{
+ 			//extract ip address and add to list
+ 			String ip = ipAddresses.substring(0, ipAddresses.indexOf(","));
+ 			listOfIpAddresses.add(ip);
+ 			//Move up to next ip address
+ 			ipAddresses = ipAddresses.substring(ipAddresses.indexOf(",")+1);
+ 			numberOfIpAddresses--;
+ 		}while(numberOfIpAddresses > 0);
+ 		
+ 		return listOfIpAddresses;
+ 	}
 }
