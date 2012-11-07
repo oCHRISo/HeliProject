251955ad83787eb048d8cce5890fe188e519c604
Chris Adams <chrisadams1267@gmail.com>
2012-11-07 23:36:34 +0000

    Adding more config infomation

---
+++ src/com/mycompany/reservationsystem/peer/data/PropertyFile.java
@@ -0,0 +1,43 @@
+package com.mycompany.reservationsystem.peer.data;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFile {
+	private static PropertyFile instance = null;
+	private Properties prop;
+	private final String propFilePath = "data\\config.properties";
+	
+	private PropertyFile(){
+		prop = new Properties();
+		try {
+			//load a properties file
+			prop.load(new FileInputStream(propFilePath));
+		} 
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static PropertyFile getInstance(){
+		if(instance == null){
+			instance = new PropertyFile();
+		}
+		
+		return instance;
+	}
+	
+	//get the property value and return
+	public int getPeerStateDeamonTime(){
+		return Integer.parseInt(prop.getProperty("ExecutePeerStateDeamon"));
+	}
+	
+	public int getExecuteTranactionDaemonTime(){
+		return Integer.parseInt(prop.getProperty("ExecuteTranactionDaemon"));
+	}
+	
+	public int getTransactionTimePeriod(){
+		return Integer.parseInt(prop.getProperty("TransactionTimePeriod"));
+	}
+}
