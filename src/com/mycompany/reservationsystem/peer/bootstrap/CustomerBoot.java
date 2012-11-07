40c2aaa1df46ae63d4c720175a5ee70b19b96231
Chris Adams <chrisadams1267@gmail.com>
2012-11-07 12:26:54 +0000

    Adding boot strap for customer

--- src/com/mycompany/reservationsystem/peer/bootstrap/CustomerBoot.java
+++ src/com/mycompany/reservationsystem/peer/bootstrap/CustomerBoot.java
@@ -1,7 +1,29 @@
 package com.mycompany.reservationsystem.peer.bootstrap;
 
-public class CustomerBoot {
-	public static void main(String[] args) {
+import com.mycompany.reservationsystem.peer.deamon.PeerStateDeamon;
+import com.mycompany.reservationsystem.peer.server.PeerServer;
+import com.mycompany.reservationsystem.peer.server.booking.BookingServer;
+import com.mycompany.reservationsystem.peer.ui.CustomerUserInterface;
 
+public class CustomerBoot {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		PeerStateDeamon peerDeamon = new PeerStateDeamon();
+		PeerServer peerServer = new PeerServer();
+		BookingServer bookingServer = new BookingServer();
+		
+		peerDeamon.setDaemon(true);
+		peerDeamon.start();
+		peerServer.start();
+		bookingServer.start();
+		
+		CustomerUserInterface cui = new CustomerUserInterface();
+		cui.showOptions();
+		
+		peerServer.close();
+		bookingServer.close();
+		peerServer.stop();
+		bookingServer.stop();
+		peerDeamon.stop();
 	}
 }
