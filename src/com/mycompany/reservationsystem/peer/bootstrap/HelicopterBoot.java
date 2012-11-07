526ccfac9579e4da968a9fa669f85eef879fc41b
Chris Adams <chrisadams1267@gmail.com>
2012-11-07 12:26:38 +0000

    Adding boot strap code for helicopter

--- src/com/mycompany/reservationsystem/peer/bootstrap/HelicopterBoot.java
+++ src/com/mycompany/reservationsystem/peer/bootstrap/HelicopterBoot.java
@@ -1,7 +1,42 @@
 package com.mycompany.reservationsystem.peer.bootstrap;
 
-public class HelicopterBoot {
-	public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+import com.mycompany.reservationsystem.peer.deamon.PeerStateDeamon;
+import com.mycompany.reservationsystem.peer.server.PeerServer;
+import com.mycompany.reservationsystem.peer.server.booking.BookingServer;
+
+public class HelicopterBoot {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		PeerStateDeamon peerDeamon = new PeerStateDeamon();
+		PeerServer peerServer = new PeerServer();
+		BookingServer bookingServer = new BookingServer();
+		
+		peerDeamon.setDaemon(true);
+		peerDeamon.start();
+		peerServer.start();
+		bookingServer.start();
+		
+		String userInput = null;
+		do{
+			System.out.println("Enter 1 to stop the program:");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			try {
+				userInput = br.readLine();
+		    } 
+			catch (IOException ioe) {
+				System.out.println("IO error trying to read your input!");
+		    }
+		}while(!userInput.equals("1"));
+		
+		peerServer.close();
+		bookingServer.close();
+		peerServer.stop();
+		bookingServer.stop();
+		peerDeamon.stop();
 	}
 }
